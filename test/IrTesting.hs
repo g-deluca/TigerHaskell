a3e2c2908d87d6c465d9d00aafee6222f06a45ec
Guido De Luca <guido.deluca7@gmail.com>
2019-09-24 18:06:21 -0300

    Agrego testsuite para código intermedio

--- test/IrTesting.hs
+++ test/IrTesting.hs
@@ -5,6 +5,7 @@ import           TigerEscap  (calcularEEsc)
 import           TigerParser (parse)
 import           TigerSeman (calcularSeman)
 import           TigerTrans (BExp)
+import           TigerPrettyIr (renderBIr)
 
 import           Tools
 
@@ -13,13 +14,19 @@ main =
   -- testGood "./test/test_code/" tester "merge.tig"
 
   putStrLn "\n==== Good loc ====" >>
-  testDir good_loc (testSTDGood tester) >>
-  putStrLn "\n==== Type Loc ====" >>
-  testDir type_loc (testBad type_loc tester)
+  testDir good_loc (testIr good_loc tester)
+
+testIr :: Show a => String -> (String -> Either a (BExp, Tipo)) -> String -> IO ()
+testIr loc = test loc ( badRes . show )
+                      ( goodRes . renderBIr . fst)
 
 tester :: String -> Either Symbol (BExp, Tipo)
 tester = either (\s -> Left s)
-                 calcularSeman
+                calcularSeman
          .  (either (fail $ "Testing Escapes: Parser error")
                    calcularEEsc
             . parse )
+
+preprocessIr :: Either Symbol (BExp, Tipo) -> Either Symbol String
+preprocessIr (Left s) = Left s
+preprocessIr (Right (bexp, _)) = Right (renderBIr bexp)
