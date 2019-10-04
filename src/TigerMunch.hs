4fa11c6079fb19ef72a0f0eb5585451ef3cdf542
Guido De Luca <guido.deluca7@gmail.com>
2019-10-04 20:05:41 -0300

    WIP on munchExp

--- src/TigerMunch.hs
+++ src/TigerMunch.hs
@@ -4,7 +4,7 @@ import Assem
 
 import TigerTemp
 import TigerUnique
-import TigerTree
+import TigerTree as T
 
 import Control.Monad.State
 
@@ -23,7 +23,202 @@ munchStm :: InstrEmitter w => Stm -> w ()
 munchStm = undefined
 
 munchExp :: InstrEmitter w => Exp -> w Temp
-munchExp = undefined
+munchExp (Const i) = do
+    r <- newTemp
+    emit $ Oper {
+        oassem = "mov d0, " ++ show i ++ "\n",
+        osrc = [],
+        odst = [r],
+        ojump = Nothing
+    }
+    return r
+munchExp (Name l) = do
+    r <- newTemp
+    emit $ Oper {
+        -- TODO: Googlear un poco más offset
+        oassem = "mov d0, OFFSET " ++ show l ++ "\n",
+        osrc = [],
+        odst = [r],
+        ojump = Nothing
+    }
+    return r
+munchExp (Temp t) = return t
+munchExp (Eseq s e) = munchStm s >> munchExp e
+munchExp (T.Mem (T.Binop T.Plus e (T.Const i))) = do
+-- TODO: ¿Por qué agarramos puntualmente el caso de "Plus" y no
+-- el resto. Así está en el libro, ¿but why?
+    res <- newTemp
+    te <- munchExp e
+    emit $ Oper {
+        oassem = "mov d0, [s0 + " ++ show i ++ "]\n",
+        osrc = [te],
+        odst = [res],
+        ojump = Nothing
+    }
+    return res
+munchExp (T.Mem (T.Binop T.Plus (T.Const i) e)) = do
+    res <- newTemp
+    te <- munchExp e
+    emit $ Oper {
+        oassem = "mov d0, [s0 + " ++ show i ++ "]\n",
+        osrc = [te],
+        odst = [res],
+        ojump = Nothing
+    }
+    return res
+munchExp (T.Mem (T.Const i)) = do
+    res <- newTemp
+    emit $ Oper {
+        oassem = "mov d0, [" ++ show i ++ "]\n",
+        osrc = [],
+        odst = [res],
+        ojump = Nothing
+    }
+    return res
+munchExp (T.Mem e) = do
+    te <- munchExp e
+    res <- newTemp
+    emit $ Oper {
+        oassem = "mov d0, [s0]\n",
+        osrc = [te],
+        odst = [res],
+        ojump = Nothing
+    }
+    return res
+
+-- Arrancamo' con Binop...
+munchExp (T.Binop T.Plus el er) = do
+    tl <- munchExp el
+    tr <- munchExp er
+    res <- newTemp
+    -- TODO: ¿Usar move?
+    emit $ Oper {
+        oassem = "mov d0, s0\n",
+        osrc = [tl],
+        odst = [res],
+        ojump = Nothing
+    }
+    emit $ Oper {
+        oassem = "add d0, s0\n",
+        osrc = [tr, res],
+        odst = [res],
+        ojump = Nothing
+    }
+    return res
+munchExp (T.Binop T.Minus el er) = do
+    tl <- munchExp el
+    tr <- munchExp er
+    res <- newTemp
+    emit $ Oper {
+        oassem = "mov d0, s0\n",
+        osrc = [tl],
+        odst = [res],
+        ojump = Nothing
+    }
+    emit $ Oper {
+        oassem = "sub d0, s0\n",
+        osrc = [tr, res],
+        odst = [res],
+        ojump = Nothing
+    }
+    return res
+-------------
+-- Pequeña optimización ;)
+munchExp (T.Binop T.Mul (Const i) er) = do
+    tr <- munchExp er
+    res <- newTemp
+    emit $ Oper {
+        oassem = "imul d0, s0, " ++ show i ++ "\n",
+        osrc = [tr],
+        odst = [res],
+        ojump = Nothing
+    }
+    return res
+munchExp (T.Binop T.Mul el (Const i)) = do
+    tl <- munchExp el
+    res <- newTemp
+    emit $ Oper {
+        oassem = "imul d0, s0, " ++ show i ++ "\n",
+        osrc = [tl],
+        odst = [res],
+        ojump = Nothing
+    }
+    return res
+-------------
+munchExp (T.Binop T.Mul el er) = do
+    tl <- munchExp el
+    tr <- munchExp er
+    res <- newTemp
+    emit $ Oper {
+        oassem = "mov d0, s0\n",
+        osrc = [tl],
+        odst = [res],
+        ojump = Nothing
+    }
+    emit $ Oper {
+        oassem = "imul d0, s0\n",
+        osrc = [tr, res],
+        odst = [res],
+        ojump = Nothing
+    }
+    return res
+munchExp (T.Binop T.Div el er) = error "COMPLETAR"
+munchExp (T.Binop T.And el er) = do
+    tl <- munchExp el
+    tr <- munchExp er
+    res <- newTemp
+    emit $ Oper {
+        oassem = "mov d0, s0\n",
+        osrc = [tl],
+        odst = [res],
+        ojump = Nothing
+    }
+    emit $ Oper {
+        oassem = "and d0, s0\n",
+        osrc = [tr, res],
+        odst = [res],
+        ojump = Nothing
+    }
+    return res
+munchExp (T.Binop T.Or el er) = do
+    tl <- munchExp el
+    tr <- munchExp er
+    res <- newTemp
+    emit $ Oper {
+        oassem = "mov d0, s0\n",
+        osrc = [tl],
+        odst = [res],
+        ojump = Nothing
+    }
+    emit $ Oper {
+        oassem = "or d0, s0\n",
+        osrc = [tr, res],
+        odst = [res],
+        ojump = Nothing
+    }
+    return res
+munchExp (T.Binop T.XOr el er) = do
+    tl <- munchExp el
+    tr <- munchExp er
+    res <- newTemp
+    emit $ Oper {
+        oassem = "mov d0, s0\n",
+        osrc = [tl],
+        odst = [res],
+        ojump = Nothing
+    }
+    emit $ Oper {
+        oassem = "xor d0, s0\n",
+        osrc = [tr, res],
+        odst = [res],
+        ojump = Nothing
+    }
+    return res
+munchExp (T.Binop T.LShift el er) = error "JAJAJAJAJ"
+munchExp (T.Binop T.RShift el er) = error "FUck off"
+munchExp (T.Binop T.ARShift el er) = error "Seriously fUck off"
+munchExp (T.Call e args) = error "COMPLETAR"
+
 
 
 -- | Definimos la mónada que instanciaremos en Emitter. Le puse Mordisco
