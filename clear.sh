549a094c3c384b2260f4b50546289f606e00e6f0
Guido De Luca <guido.deluca7@gmail.com>
2020-08-30 12:19:14 -0300

    Adding extension to exectuables so that we can ignore them on git

---
+++ clear.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo -e "\e[36mRemoved executables\e[0m"
+
+rm *.out
--- run.sh
+++ run.sh
@@ -3,9 +3,9 @@
 echo -e "\e[36mCompile and run \e[1m$1\e[0m"
 
 rm "./assem/$1.s"
-rm "$1"
+rm "$1.out"
 stack run -- "./test/test_code/good/$1.tig" >> "./assem/$1.s"
-gcc -m32 runtime.c "./assem/$1.s" -o "$1"
+gcc -m32 runtime.c "./assem/$1.s" -o "$1.out"
 
-./$1
+./$1.out
 echo -e "\nreturn: $?"
